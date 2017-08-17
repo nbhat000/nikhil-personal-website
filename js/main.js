0083184f6bf98e40cea929591dc299cb57d63fd9
Bhat <mmm269@dca90475f25b.cof.ds.capitalone.com>
2017-08-17 13:28:27 -0400

    refactored with bootstrap

---
+++ js/main.js
@@ -0,0 +1,20 @@
+window.onload = function() {
+    // var feed = new Instafeed({
+    //     get: 'user',
+    //     userId:  4978928504,
+    //     target: 'instagram',
+		//     resolution: 'standard_resolution',
+    //     // tagName: 'awesome',
+    //     clientId: 'bb584ea742d746b4a5920008181d7292'
+    // });
+    // feed.run();
+    console.log("JS Loaded");
+    var feed = new Instafeed({
+        get: 'tagged',
+        tagName: 'awesome',
+        accessToken: '94764.1677ed0.c6256a27eddf41709ddf29af3469a4e5',
+        clientId: 'bb584ea742d746b4a5920008181d7292'
+    });
+    console.log(feed);
+    feed.run();
+}
